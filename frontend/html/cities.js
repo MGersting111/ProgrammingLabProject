d229233a487809d1cfed862a6d3534acebdbcf81
fa-r-ah <farah.darabyou@stud.fra-uas.de>
2024-05-18 12:32:04 +0200

    frontend design aber dropdowns wollen immer noch nicht funktionieren

---
+++ frontend/html/cities.js
@@ -0,0 +1,8 @@
+const cities = [
+    { "city": "Tuscarora", "state": "Nevada" },
+    { "city": "Sunol", "state": "California" },
+    { "city": "Esparto", "state": "California" },
+    { "city": "Cibola", "state": "Arizona" },
+    { "city": "Stockton", "state": "California" },
+    { "city": "Arbuckle", "state": "California" },
+    { "city": "Mount Hamilton", "state": "California" } ]
